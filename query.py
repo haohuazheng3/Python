2e19ac3fddc0d4d83e8a1b6504e385c569bcc142
Haohua <haohuazheng001@gmail.com>
2023-10-31 14:08:46 -0400

    Implemented cast-based movie search

--- query.py
+++ query.py
@@ -6,6 +6,7 @@ def display_menu():
     print("Menu:")
     print("1. List all movie names")
     print("2. Search the movie name")
+    print("3. Search movies by cast")
     print("q. Quit")
     return input("Please select an option: ")
 
@@ -27,6 +28,17 @@ def search_movie_by_name():
     else:
         print("No movies found with the specified word.")
 
+def search_movie_by_cast():
+    keyword = input("Enter the word to search for in movie casts: ")
+    results = movies.search_movies_by_cast(keyword)
+    if results:
+        for movie in results:
+            print(movie['name'])
+            print(movie['cast'])
+    else:
+        print("No movies found with the specified word in the casts.")
+
+
 if __name__ == "__main__":
     while True:
         choice = display_menu()
@@ -36,5 +48,7 @@ if __name__ == "__main__":
             list_all_movie_names()
         elif choice == '2': 
             search_movie_by_name()
+        elif choice == '3': 
+            search_movie_by_cast()
         else:
             print("\nInvalid option. Please try again.\n")
